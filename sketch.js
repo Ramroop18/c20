02493fbeda6054f78026c20be994fdc77ac334e7
Ramroop18 <73176457+Ramroop18@users.noreply.github.com>
2020-10-31 18:40:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var fixedrect,movingrect
+function setup() {
+  createCanvas(800,400);
+  fixedrect=createSprite(400, 200, 50, 50);
+  movingrect=createSprite(200,200,43,35);
+}
+
+function draw() {
+  background(255,255,255); 
+  movingrect.x=mouseX
+  movingrect.y=mouseY
+  if(fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2 &&
+    movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2 &&
+    fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2 &&
+    movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2){
+      fixedrect.shapeColor="blue"
+      movingrect.shapeColor="blue"
+    }
+    else{fixedrect.shapeColor="red"
+  movingrect.shapeColor="red"}
+  drawSprites();
+}
